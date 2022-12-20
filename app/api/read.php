c771444a0fc961063cca5a7439ab7506c6ca8bc6
Gorguluo <119026050+Gorguluo@users.noreply.github.com>
2022-12-20 16:34:34 -0500

    Create read.php

---
+++ app/api/read.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+ 
+include_once '../config/database.php';
+ 
+$ch = curl_init();
+ 
+$source = isset($_GET['sources']) && !empty($_GET['sources']) ? $_GET['sources'] : '';
+$category = isset($_GET['categories']) && !empty($_GET['categories']) ? $_GET['categories'] : '';
+$country = isset($_GET['countries']) && !empty($_GET['countries']) ? $_GET['countries'] : '';
+ 
+$apiUrl = 'https://api.mediastack.com/v1/news?access_key=e51b8819364d71188fbdfc28e7a9777a&limit=100&languages=en&date=' . date("Y-m-d");
+if (!empty($country)) {
+    $apiUrl .= '&countries=' . $country;
+}
+if (!empty($source)) {
+    $apiUrl .= '&sources=' . $source;
+}
+if (!empty($category)) {
+    $apiUrl .= '&categories=' . $category;
+}
+ 
+curl_setopt($ch, CURLOPT_URL, $apiUrl);
+ 
+$result = curl_exec($ch);
+ 
+curl_close($ch);
